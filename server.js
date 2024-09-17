0cf82ed074dcdc2d6f602b6553bb52d4f2a3c8b8
IshikaAnand7 <anandishika07@gmail.com>
2024-09-17 16:53:37 +1000

    Login page v1

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'views', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'views', 'login.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
