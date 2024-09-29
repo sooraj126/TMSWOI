b8fc6c4bfee8b5dcd9dd79a705a7f4b45158b650
tharinduLTM <s224238367@deakin.edu.au>
2024-09-29 16:53:56 +1000

    Adding task.modal

---
+++ backend/Modal/task.modal.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  deadline: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: 'N/A'
+  },
+  priority: {
+    type: String,
+    enum: ['high', 'medium', 'low'],
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['personal', 'work'],
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['notStarted', 'inProgress', 'onHold', 'completed'],
+    default: 'notStarted'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  collection: 'Task'
+});
+
+module.exports = mongoose.model('Task', taskSchema);
