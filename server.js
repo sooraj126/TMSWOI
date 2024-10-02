df1e0ccdbc7ab91d34d585526a935013dfeafb12
IshikaAnand7 <anandishika07@gmail.com>
2024-10-02 11:57:07 +1000

    fix login test

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.get('/task', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3900;
+const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'test') {
-    const PORT = process.env.PORT || 3900;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
